4ae84163ca6aca6c7d34feded71ff6332b827f39
William Pasternak <31456405+WilliamPasternak@users.noreply.github.com>
2022-09-14 13:35:46 -0400

    Initial Commit

---
+++ main.js
@@ -0,0 +1,6 @@
+document.querySelector('#button').addEventListener('click',convert)
+
+function convert(){
+    document.querySelector('#letter').innerText = document.querySelector('#text').value + '\n * Leon Noel '
+}
+
